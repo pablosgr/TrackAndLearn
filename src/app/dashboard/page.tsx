4330b8fdc399844704cf1def45fa1904ad06f251
Pablo <pablosgar96@gmail.com>
2025-07-15 12:38:01 +0200

    Frontend | Add provisional login and signup forms for auth testing

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,32 @@
+'use client'
+
+import { useEffect, useState } from "react";
+import { signOut } from "../actions";
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+
 export default function Dashboard() {
-    return (
-        <div className="flex flex-row justify-items-center">
-            <p>This is <strong>Dashboard</strong> page</p>
-        </div>
-    )
+    const [user, setUser] = useState<User | null>(null);
+
+    useEffect(() => {
+        const supabase = createClient();
+        supabase.auth.getUser().then(({ data, error }) => {
+            if (error || !data?.user) {
+                window.location.href = '/';
+            } else {
+                setUser(data.user);
+            }
+        });
+    }, []);
+
+    const handleSignOut = async () => {
+        const result = await signOut();
+    }
+
+  return (
+    <div className="fles flex-row p-10 gap-12">
+        <p>Hello {user?.email}</p>
+        <button onClick={handleSignOut} className="p-4 rounded-lg bg-amber-700/50 hover:cursor-pointer hover:bg-amber-600/50 text-white transition-colors">Sign Out</button>
+    </div>
+  )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,51 @@
+'use client';
+
+import { FormEvent, useState } from "react";
+import { login, signUp } from "./actions";
+
 export default function Login() {
+  const [error, setError] = useState<string | null>(null);
   
+  const handleLogIn = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const result = await login(formData);
+    if (result.error) {
+      setError(result.error);
+    }
+  }
+
+  const handleSignUp = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const result = await signUp(formData);
+    if (result.error) {
+      setError(result.error);
+    }
+  }
+
   return (
-    <div className="flex flex-col justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p>Login form</p>
+    <div className="flex flex-row gap-12 items-center justify-center min-h-screen min-w-screen p-8 pb-20 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <form className="flex flex-col gap-3 p-8 shadow-2xl h-fit rounded-xl" onSubmit={handleLogIn}>
+        {
+          error && <p className="text-red-500">{error}</p>
+        }
+        <input name="email" type="email" placeholder="Email" className="border-gray-400 border-1 p-2 rounded-lg" required />
+        <input name="password" type="password" placeholder="Password" className="border-gray-400 border-1 p-2 rounded-lg" required />
+        <button className="p-3 bg-gray-600 hover:cursor-pointer hover:bg-gray-400 text-white rounded-lg transition-colors">Log in</button>
+      </form>
+      <form className="flex flex-col gap-3 p-8 shadow-2xl h-fit rounded-xl" onSubmit={handleSignUp}>
+        <input  name="name" type="text" placeholder="Your name" className="border-gray-400 border-1 p-2 rounded-lg" required />
+        <input name="username" type="text" placeholder="Your username" className="border-gray-400 border-1 p-2 rounded-lg" required />
+        <input name="email" type="email" placeholder="Your email" className="border-gray-400 border-1 p-2 rounded-lg" required />
+        <input name="password" type="password" placeholder="Your password" className="border-gray-400 border-1 p-2 rounded-lg" required />
+        <select name="role" className="border-gray-400 border-1 p-2 rounded-lg" required>
+          <option value="">Choose your role</option>
+          <option value="teacher">I'm a teacher</option>
+          <option value="student">I'm a student</option>
+        </select>
+        <button className="p-3 bg-gray-600 hover:cursor-pointer hover:bg-gray-400 text-white rounded-lg transition-colors">Sign up</button>
+      </form>
     </div>
   );
 }
