c51afa2b06e94be15f8d6e150861f644baab9cdb
Pablo <pablosgar96@gmail.com>
2025-07-30 11:31:04 +0200

    Frontend | Tests | Set base card component for TestDetail page

--- src/app/(protected)/tests/[id]/TestDetailClient.tsx
+++ src/app/(protected)/tests/[id]/TestDetailClient.tsx
@@ -1,5 +1,13 @@
 import { TestType } from "@/types/test/TestType";
 import TestTemplateType from "@/types/test/TestTemplateType";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
 
 export default async function TestDetailClient({ 
     testList,
@@ -10,34 +18,11 @@ export default async function TestDetailClient({
 }) {
 
     return (
-        <div className="flex flex-col gap-5 p-10 m-10 shadow-xl rounded-lg bg-amber-200/50">
-            {
-                testList && testList.map((test) => (
-                    <section key={test.id} className="p-5 bg-emerald-300 rounded-lg">
-                        <h2>{test.name}</h2>
-                        <p>{test.level}</p>
-                        {
-                            test.adaptation_data && <p>Adaptation: {test.adaptation_data.name}</p>
-                        }
-                        <ul className="flex flex-col gap-8 mt-8">
-                            {
-                                test.question.map((q) => (
-                                    <li key={q.id} className="p-5 bg-white rounded-xl">
-                                        <p className="font-semibold">{q.question_text}</p>
-                                        {
-                                            q.option.map((o) => (
-                                                <p key={o.id} className={`ml-4 ${o.is_correct && 'text-green-500'}`}>
-                                                    {o.option_text}
-                                                </p>
-                                            ))
-                                        }
-                                    </li>
-                                ))
-                            }
-                        </ul>
-                    </section>
-                ))
-            }
-        </div>
+        <Card>
+            <CardHeader className="bg-(--color-accent)">
+                <CardTitle className="text-2xl">{testTemplate.name} | {testTemplate.topic_data.name}</CardTitle>
+                <CardDescription>Created on {new Date(testTemplate.created_at).toLocaleDateString()}</CardDescription>
+            </CardHeader>
+        </Card>
     );
 }
---
+++ src/components/tests/TestDetailCard.tsx
@@ -0,0 +1,71 @@
+import { deleteTestTemplateById } from "@/app/(protected)/tests/actions/delete";
+import TestTemplateType from "@/types/test/TestTemplateType";
+import Link from "next/link";
+import { Trash } from "lucide-react";
+import { Button } from "../ui/button";
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+export default function TestDetailCard({ test, onDelete }: { test: TestTemplateType, onDelete: (id: string) => void }) {
+
+    const handleDelete = async (e: React.MouseEvent) => {
+        e.stopPropagation();
+        onDelete(test.id);
+        await deleteTestTemplateById(test.id);
+    }
+
+    return (
+        <Card className="w-90">
+            <CardHeader className="bg-(--color-accent) truncate">
+                <CardTitle className="text-lg truncate">{test.name}</CardTitle>
+                <AlertDialog>
+                    <AlertDialogTrigger asChild>
+                        <CardAction className="p-2 hover:bg-(--color-destructive) hover:cursor-pointer rounded-lg transition-colors">
+                            <Trash size={22}/>
+                        </CardAction>
+                    </AlertDialogTrigger>
+                    <AlertDialogContent>
+                        <AlertDialogHeader>
+                            <AlertDialogTitle>Are you sure?</AlertDialogTitle>
+                            <AlertDialogDescription>
+                                Test template and all related tests will be permanently removed. This action cannot be undone.
+                            </AlertDialogDescription>
+                        </AlertDialogHeader>
+                        <AlertDialogFooter>
+                            <AlertDialogCancel>Cancel</AlertDialogCancel>
+                            <AlertDialogAction onClick={handleDelete}>Delete</AlertDialogAction>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialog>
+                <CardDescription>Topic: {test.topic_data.name}</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-row justify-between gap-8 items-center">
+                <span className="truncate text-sm">
+                    Created on {new Date(test.created_at).toLocaleDateString()}
+                </span>
+                <Link href={`/tests/${test.id}`}>
+                    <Button variant={'outline'} className="ml-6">
+                        Go to test
+                    </Button>
+                </Link>
+            </CardContent>
+        </Card>
+    )
+}
