e6f67563a9519e192b2db5c2ef9369623e8eb6b8
Pablo <pablosgar96@gmail.com>
2025-08-03 14:37:00 +0200

    Backend | Tests | Add adaptation type and refactor test type for reusability

---
+++ src/types/test/AdaptationType.d.ts
@@ -0,0 +1,6 @@
+export type AdaptationType = {
+    id: number;
+    name: string;
+    code: string;
+    description: string | null;
+}
--- src/types/test/TestType.d.ts
+++ src/types/test/TestType.d.ts
@@ -1,21 +1,24 @@
 import { QuestionType } from "./QuestionType";
 
-export type TestTemplateRef = {
-    teacher_id: number;
-};
-
-export type TestType = {
-    id: number;
+export type NewTestType = {
     template_id: number,
     name: string;
     level: string | null;
     time_limit: number | null;
     adaptation_id: number | null;
+}
+
+export type TestTemplateRef = {
+    teacher_id: number;
+};
+
+export type TestType = NewTestType & {
+    id: number;
+    created_at: string;
+    test_template: TestTemplateRef | null;
+    question: QuestionType[];
     adaptation_data: {
         name: string;
         code: string;
     } | null;
-    created_at: string;
-    test_template: TestTemplateRef | null;
-    question: QuestionType[];
 };
