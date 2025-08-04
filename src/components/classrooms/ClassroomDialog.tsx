7cac6ae6b15908d1e672f595ce77ae6eee07c3f8
Pablo <pablosgar96@gmail.com>
2025-08-04 19:38:09 +0200

    Frontend | Classrooms | Add classroom dialog for creation and edition

---
+++ src/components/classrooms/ClassroomDialog.tsx
@@ -0,0 +1,144 @@
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import { PencilLine } from "lucide-react";
+import { useUser } from "../context/userWrapper";
+import { ClassroomType } from "@/types/classroom/ClassroomType";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { CardAction } from "../ui/card";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
+
+
+const formSchema = z.object({
+    name: z.string().min(3, { message: 'Name must have between 3 and 150 characters' }).max(150),
+});
+
+export default function ClassroomDialog({
+    type,
+    classroom,
+    onCreate,
+    onUpdate,
+}: {
+    type: 'update' | 'create',
+    classroom?: ClassroomType,
+    onCreate?: (newClassroom: ClassroomType) => void,
+    onUpdate?: (updatedClassroom: ClassroomType) => void
+}) {
+    const router = useRouter();
+    const user = useUser();
+    const [open, setOpen] = useState<boolean>(false);
+
+    const defaultClassroom = classroom ?? {
+        id: 0,
+        teacher_id: user.id,
+        name: '',
+    }
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: defaultClassroom.name,
+        },
+    });
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        if (type === 'create' && onCreate) {
+            // const newClassroom = await createClassroom(values.name, defaultClassroom.teacher_id);
+
+            // if (newClassroom) {
+            //     onCreate(newClassroom);
+            //     form.reset();
+            // }
+        }
+
+        if (type === 'update' && onUpdate) {
+
+        }
+
+        setOpen(false);
+        router.refresh();
+    }
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogTrigger asChild>
+                {
+                    type === 'create'
+                    ? <Button variant="default">Add Classroom</Button>
+                    : <CardAction className="p-2 hover:bg-cyan-100 hover:cursor-pointer rounded-lg transition-colors">
+                        <PencilLine size={22} />
+                    </CardAction>
+                }
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <DialogHeader>
+                    <DialogTitle>
+                        {
+                            type === 'create'
+                            ? 'Add new classroom'
+                            : 'Edit classroom'
+                        }
+                    </DialogTitle>
+                    <DialogDescription className="pt-2">
+                        {
+                            type === 'create'
+                            ? 'This action will create a new classroom'
+                            : 'Edit classroom information'
+                        }
+                    </DialogDescription>
+                </DialogHeader>
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-6">
+                        <FormField
+                            control={form.control}
+                            name="name"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>
+                                        Name
+                                    </FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="Name your template" {...field}/>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <DialogFooter className="pt-4">
+                            <DialogClose asChild>
+                                <Button variant="outline" onClick={() => form.clearErrors()}>Cancel</Button>
+                            </DialogClose>
+                            <Button type="submit">
+                                {
+                                    type === 'create'
+                                    ? 'Create classroom'
+                                    : 'Save changes'
+                                }
+                            </Button>
+                        </DialogFooter>
+                    </form>
+                </Form>
+            </DialogContent>
+        </Dialog>
+    )
+}
