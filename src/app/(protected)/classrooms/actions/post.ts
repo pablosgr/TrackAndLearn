15db0859f8a20fbd1549790e48979b4b88385e91
Pablo <pablosgar96@gmail.com>
2025-08-04 19:44:50 +0200

    Backend | Classrooms | Add classroom creation action

---
+++ src/app/(protected)/classrooms/actions/post.ts
@@ -0,0 +1,24 @@
+'use server'
+
+import { createClient } from "@/utils/supabase/server";
+import { ClassroomType } from "@/types/classroom/ClassroomType";
+
+export async function createClassroom(name: string, teacherId: string): Promise<ClassroomType | null> {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+        .from('classroom')
+        .insert({
+            name: name,
+            teacher_id: teacherId
+        })
+        .select()
+        .single();
+
+    if (!data || error) {
+        console.error('Error inserting new classroom: ', error);
+        return null;
+    }
+
+    return data as ClassroomType;
+}
