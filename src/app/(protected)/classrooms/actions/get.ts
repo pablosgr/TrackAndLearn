1ff34f988a72f57c6d5fac46a6789b41413cbc0e
Pablo <pablosgar96@gmail.com>
2025-08-10 13:15:47 +0200

    Backend | Classrooms | Add student adaptation retrieving action

---
+++ src/app/(protected)/classrooms/actions/get.ts
@@ -0,0 +1,21 @@
+'use server'
+
+import { createClient } from "@/utils/supabase/server";
+
+export async function getStudentIdAdaptation(classroomId: string, studentId: string): Promise<string | null> {
+    const supabase = await createClient();
+
+    const { data, error } = await supabase
+        .from('student_classroom')
+        .select('adaptation_id')
+        .eq('classroom_id', classroomId)
+        .eq('student_id', studentId)
+        .single();
+
+    if (!data || error) {
+        console.error('Error retrieving student adaptation: ', error);
+        return null;
+    }
+
+    return data.adaptation_id;
+}
