4c3c5fc9c8275e8605c5a96c060347638e397662
Pablo <pablosgar96@gmail.com>
2025-08-11 18:33:14 +0200

    Frontend | Classrooms | Add average score chart component

--- src/app/(protected)/classrooms/[id]/test/[testId]/result/ClassroomResultClient.tsx
+++ src/app/(protected)/classrooms/[id]/test/[testId]/result/ClassroomResultClient.tsx
@@ -1,17 +1,21 @@
 import StudentsResultAccordion from "@/components/classrooms/StudentsResultAccordion";
+import AverageScoreCard from "@/components/classrooms/AverageScoreCard";
 import { ClassroomResultType } from "@/types/test/ClassroomResultType";
 
 export default function ClassroomResultClient({ classroomResults }: { classroomResults: ClassroomResultType[] }) {
     return (
         <>
             <h1>Results</h1>
-            <section className="w-full flex">
-                <h2 className="text-xl mb-5">Classroom result</h2>
-            </section>
-            <section className="w-full flex flex-col gap-3">
-                <h2 className="text-xl mb-5">Individual results</h2>
-                <StudentsResultAccordion classroomResults={classroomResults} />
-            </section>
+            <div className="flex flex-col gap-10 w-full">
+                <section className="w-full flex flex-col gap-3">
+                    <h2 className="text-xl mb-5">Classroom result</h2>
+                    <AverageScoreCard classroomResults={classroomResults} />
+                </section>
+                <section className="w-full flex flex-col gap-3">
+                    <h2 className="text-xl mb-5">Individual results</h2>
+                    <StudentsResultAccordion classroomResults={classroomResults} />
+                </section>
+            </div>
         </>
     )
 }
---
+++ src/components/classrooms/AverageScoreCard.tsx
@@ -0,0 +1,132 @@
+'use client';
+
+import type { ClassroomResultType } from "@/types/test/ClassroomResultType";
+import { Label, Pie, PieChart } from "recharts";
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card";
+
+export default function AverageScoreCard({
+    classroomResults
+} : {
+    classroomResults: ClassroomResultType[]
+}) {
+    let count = 0;
+    let scoreSum = 0;
+    const highScores = [];
+    const midScores = [];
+    const lowScores = [];
+
+    classroomResults.forEach((i) => {
+        count ++;
+        scoreSum += i.score;
+
+        if (i.score < 5) {
+            lowScores.push(i);
+        } else if (i.score < 7) {
+            midScores.push(i);
+        } else {
+            highScores.push(i);
+        }
+    });
+
+    const averageScore = scoreSum / count;
+
+    const chartData = [
+        { score: "high", students: highScores.length, fill: "#27F554" },
+        { score: "mid", students: midScores.length, fill: "#EEF527" },
+        { score: "low", students: lowScores.length, fill: "tomato" }
+    ];
+    
+    const chartConfig = {
+        students: {
+            label: "Students",
+        },
+        high: {
+            label: "High (7 to 10)",
+            color: "var(--chart-1)",
+        },
+        mid: {
+            label: "Mid (5 to 7)",
+            color: "var(--chart-2)",
+        },
+        low: {
+            label: "Low (0 to 5)",
+            color: "var(--chart-3)",
+        }
+    } satisfies ChartConfig
+
+    return (
+        <Card className="flex flex-col w-full">
+            <CardHeader className="items-center">
+                <CardTitle>Average Score</CardTitle>
+                <CardDescription>Score is calculated out of 10</CardDescription>
+            </CardHeader>
+            <CardContent className="flex-1 pb-0">
+                <ChartContainer
+                    config={chartConfig}
+                    className="mx-auto aspect-square max-h-[250px]"
+                >
+                    <PieChart>
+                        <ChartTooltip
+                            cursor={false}
+                            content={<ChartTooltipContent hideLabel />}
+                        />
+                        <Pie
+                            data={chartData}
+                            dataKey="students"
+                            nameKey="score"
+                            innerRadius={60}
+                            strokeWidth={5}
+                        >
+                            <Label
+                                content={({ viewBox }) => {
+                                if (viewBox && "cx" in viewBox && "cy" in viewBox) {
+                                    return (
+                                        <text
+                                            x={viewBox.cx}
+                                            y={viewBox.cy}
+                                            textAnchor="middle"
+                                            dominantBaseline="middle"
+                                        >
+                                            <tspan
+                                                x={viewBox.cx}
+                                                y={viewBox.cy}
+                                                className="fill-foreground text-3xl font-bold"
+                                            >
+                                            {averageScore.toFixed(2)}
+                                            </tspan>
+                                            <tspan
+                                                x={viewBox.cx}
+                                                y={(viewBox.cy || 0) + 24}
+                                                className="fill-muted-foreground"
+                                                >
+                                                Average Score
+                                            </tspan>
+                                        </text>
+                                    )
+                                }
+                                }}
+                            />
+                        </Pie>
+                    </PieChart>
+                </ChartContainer>
+            </CardContent>
+            <CardFooter className="text-sm self-center text-gray-500">
+                <p>*Based on {count} recorded results</p>
+            </CardFooter>
+        </Card>
+    )
+}
