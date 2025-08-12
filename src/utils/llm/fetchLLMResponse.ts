fd35ffce40f9cbb598a7ebf8d59286ad8ea9208e
Pablo <pablosgar96@gmail.com>
2025-08-12 19:47:47 +0200

    Frontend | Tests | Add function to fetch LLM response

---
+++ src/utils/llm/fetchLLMResponse.ts
@@ -0,0 +1,22 @@
+import { GoogleGenAI } from "@google/genai";
+
+const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
+const ai = new GoogleGenAI({ apiKey: apiKey });
+
+export async function fetchLLMResponse(prompt: string): Promise<string | null> {
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: prompt,
+    });
+
+    if (!response.text) {
+        return null;
+    }
+    
+    const cleanedResponse = response.text
+      .replace(/```json\s*/g, "")
+      .replace(/```\s*/g, "")
+      .trim();
+
+    return cleanedResponse;
+}
