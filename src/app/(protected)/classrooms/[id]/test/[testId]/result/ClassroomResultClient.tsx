9a56bff6e736c03bae60cbc47212afbe66843f89
Pablo <pablosgar96@gmail.com>
2025-08-12 10:57:33 +0200

    Frontend | Classrooms | Add and implement results organizer function

--- src/app/(protected)/classrooms/[id]/test/[testId]/result/ClassroomResultClient.tsx
+++ src/app/(protected)/classrooms/[id]/test/[testId]/result/ClassroomResultClient.tsx
@@ -1,23 +1,29 @@
 import StudentsResultAccordion from "@/components/classrooms/StudentsResultAccordion";
 import AverageScoreCard from "@/components/classrooms/AverageScoreCard";
 import QuestionsResultsCard from "@/components/classrooms/QuestionsResultsCard";
+import { organizeResultsByAdaptation } from "@/utils/tests/organizeResultsByAdaptation";
 import { ClassroomResultType } from "@/types/test/ClassroomResultType";
 
 export default function ClassroomResultClient({ classroomResults }: { classroomResults: ClassroomResultType[] }) {
+    const organizedResults = organizeResultsByAdaptation(classroomResults);
+
     return (
-        <>
-            <h1>Results</h1>
-            <div className="flex flex-col gap-10 w-full">
-                <section className="w-full flex flex-col gap-3">
-                    <h2 className="text-xl mb-5">Classroom result</h2>
-                    <AverageScoreCard classroomResults={classroomResults} />
-                    <QuestionsResultsCard classroomResults={classroomResults} />
-                </section>
-                <section className="w-full flex flex-col gap-3">
-                    <h2 className="text-xl mb-5">Individual results</h2>
-                    <StudentsResultAccordion classroomResults={classroomResults} />
-                </section>
-            </div>
-        </>
+        <div className="flex flex-col gap-17 w-full">
+            <section className="w-full flex flex-col gap-10">
+                <h2 className="text-3xl">Classroom result</h2>
+                <AverageScoreCard classroomResults={classroomResults} />
+                <QuestionsResultsCard classroomResults={organizedResults.find(([id]) => id === 0)?.[1] ?? []} />
+                {
+                    organizedResults.map((res, index) => {
+                        return res[0] !== 0
+                        && <QuestionsResultsCard key={index} classroomResults={res[1]} />
+                    })
+                }
+            </section>
+            <section className="w-full flex flex-col gap-3">
+                <h2 className="text-3xl mb-5">Individual results</h2>
+                <StudentsResultAccordion classroomResults={classroomResults} />
+            </section>
+        </div>
     )
 }
---
+++ src/utils/tests/organizeResultsByAdaptation.ts
@@ -0,0 +1,24 @@
+import { ClassroomResultType } from "@/types/test/ClassroomResultType";
+
+export function organizeResultsByAdaptation(classroomResults: ClassroomResultType[]): [number, ClassroomResultType[]][] {
+    const existingAdaptations: number[] = [];
+    const organizedResults: [number, ClassroomResultType[]][] = [];
+
+    classroomResults.forEach((res) =>{
+        const adaptationId = res.test_data.adaptation_id ?? 0;
+
+        adaptationId
+        ? (!existingAdaptations.includes(adaptationId) && existingAdaptations.push(adaptationId))
+        : ''
+    });
+
+    const notAdapted = classroomResults.filter((res) => res.test_data.adaptation_id === null);
+    organizedResults.push([0, notAdapted]);
+
+    existingAdaptations.forEach((adp) => {
+        const adaptedResults = classroomResults.filter((res) => res.test_data.adaptation_id === adp);
+        organizedResults.push([adp, adaptedResults]);
+    });
+
+    return organizedResults;
+}
