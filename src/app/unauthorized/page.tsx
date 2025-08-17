5236569852b40b6c438379fd15bf27692e85d920
Pablo <pablosgar96@gmail.com>
2025-08-17 13:02:57 +0200

    Frontend | Improve unauthorized page visuals

--- src/app/unauthorized/page.tsx
+++ src/app/unauthorized/page.tsx
@@ -1,11 +1,19 @@
-
 import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
 
 export default function Unauthorized() {
     return (
-        <div className="flex flex-col gap-10">
-            <p>You don't have permission to view this page</p>
-            <Link href='/dashboard' className="py-3 px-6 w-fit bg-cyan-500 text-white rounded-lg hover:cursor-pointer hover:bg-cyan-600 transition-colors">Return to Dashboard</Link>
+        <div className="w-full h-screen flex items-center justify-center">
+            <div className="h-20 flex flex-row gap-5 items-center">
+                <p>You don't have permission to view this page</p>
+                <Separator orientation="vertical" className="bg-gray-300" />
+                <Link href='/dashboard'>
+                    <Button className="py-6 px-6 text-md">
+                        Return to Dashboard
+                    </Button>
+                </Link>
+            </div>
         </div>
     )
 }
