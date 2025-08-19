ae57c5afe83ff85c85f310f8bb6f1ca0b4c3b3a9
Pablo <pablosgar96@gmail.com>
2025-08-19 21:01:57 +0200

    Backend | Profile | Add password update action

--- src/app/(protected)/profile/actions/update.ts
+++ src/app/(protected)/profile/actions/update.ts
@@ -2,6 +2,7 @@
 
 import { createClient } from "@/utils/supabase/server";
 import { UserContextType } from "@/types/context/UserContextType";
+import { hashPassword } from "@/utils/general/hashPassword";
 
 export async function updateUserById(
     userId: string,
@@ -22,4 +23,43 @@ export async function updateUserById(
     }
 
     return data;
+}
+
+export async function updatePassword(
+    userId: string,
+    email: string,
+    password: string,
+    newPassword: string
+) {
+    const supabase = await createClient();
+
+    const { error: loginError } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+    });
+
+    if (loginError) {
+        throw new Error('Incorrect password');
+    }
+
+    const { error } = await supabase.auth.updateUser({
+        password: newPassword
+    })
+
+    if (error) {
+        console.error(error);
+        throw new Error('Error updating password');
+    }
+
+    const hashedPassword = await hashPassword(newPassword);
+
+    const { error: updateError } = await supabase
+        .from('users')
+        .update({ password_hash: hashedPassword })
+        .eq('id', userId);
+
+    if (updateError) {
+        console.error(updateError);
+        throw new Error('Error updating hashed password');
+    }
 }
