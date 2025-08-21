6c389eab5bc0b5d5bb282b8e59a024c1d9320402
Pablo <pablosgar96@gmail.com>
2025-08-21 11:32:54 +0200

    Backend | Auth | Add reset forgotten password action

---
+++ src/app/auth/actions.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { createClient } from "@/utils/supabase/server";
+import { hashPassword } from "@/utils/general/hashPassword";
+
+export async function resetPassword(newPassword: string, email: string) {
+    const supabase = await createClient();
+
+    const { error } = await supabase.auth.updateUser({
+        password: newPassword
+    })
+
+    if (error) {
+        console.error(error);
+        throw new Error('Error reseting password');
+    }
+
+    const hashedPassword = await hashPassword(newPassword);
+
+    const { error: updateError } = await supabase
+        .from('users')
+        .update({ password_hash: hashedPassword })
+        .eq('email', email);
+
+    if (updateError) {
+        console.error(updateError);
+        throw new Error('Error reseting hashed password');
+    }
+}
