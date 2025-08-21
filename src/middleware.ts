b7a6d4d641246154df6ee09ef581e0ead5e71007
Pablo <pablosgar96@gmail.com>
2025-08-21 12:56:32 +0200

    Frontend | Fix | Add auth route to middleware matcher

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,23 +1,12 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest } from 'next/server';
 import { updateSession } from '@/utils/supabase/middleware';
 
-const PUBLIC_PATHS = [
-  'auth/forgot-password',
-  '/'
-]
-
 export async function middleware(request: NextRequest) {
-  const { pathname } = request.nextUrl;
-
-  if (PUBLIC_PATHS.some(path => pathname.startsWith(path))) {
-    return NextResponse.next();
-  }
-
   return await updateSession(request);
 }
 
 export const config = {
   matcher: [
-    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+    '/((?!_next/static|_next/image|favicon.ico|auth/forgot-password|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
   ],
 }
