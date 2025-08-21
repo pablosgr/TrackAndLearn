29ffe503cc5d49f2f207a4512795285e127b1b3b
Pablo <pablosgar96@gmail.com>
2025-08-21 11:34:28 +0200

    Frontend | Middleware | Add auth routes as exceptions

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,19 @@
-import { type NextRequest } from 'next/server'
-import { updateSession } from '@/utils/supabase/middleware'
+import { NextRequest, NextResponse } from 'next/server';
+import { updateSession } from '@/utils/supabase/middleware';
+
+const PUBLIC_PATHS = [
+  'auth/forgot-password',
+  '/'
+]
 
 export async function middleware(request: NextRequest) {
-  return await updateSession(request)
+  const { pathname } = request.nextUrl;
+
+  if (PUBLIC_PATHS.some(path => pathname.startsWith(path))) {
+    return NextResponse.next();
+  }
+
+  return await updateSession(request);
 }
 
 export const config = {
